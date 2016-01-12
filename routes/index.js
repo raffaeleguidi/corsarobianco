73057d0646e4c7c7f798aba09d63b8898eb8a80c
raffaele guidi <raffaele.p.guidi@gmail.com>
2016-01-12 01:12:16 +0100

    ordered by seeds number desc

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,29 @@
 var _ = require("underscore");
 var corsaronero = require("corsaronero");
+var mediaType = ["game", "movie", "music", "series"];
 
 exports.index = function(req, res){
+    function myRender(q, results, res) {
+        results =_.sortBy(results, function(item) {
+            return item.seeds ? -item.seeds : 0;
+        });
+        res.render('index', { q: q, results: results });
+    }
     if (req.query.q) {
-        console.log("q=%s", req.query.q);
         corsaronero.search(req.query.q, function(results) {
-            res.render('index', { q: req.query.q, results: results });
+            myRender(req.query.q, results, res);
+//            res.render('index', { q: req.query.q, results: results });
         })
     } else {
-        corsaronero.latestAdditions(function(results) {
-            res.render('index', { q: null, results: results });
+        corsaronero.latestAdditionsFor(1, function(results) {
+            myRender(req.query.q, results, res);
+            //res.render('index', { q: null, results: results });
         })
     }
 };
 
 exports.info = function(req, res) {
     corsaronero.info(req.query.url, function(details){
-        console.log(details);
         res.render('info', { q: req.query.q, detailsUrl: req.query.url, filename: req.query.filename, magnetUrl: details.magnetUrl });
     });
 }
